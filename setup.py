846473f91ebe762e2f2b8b3bba4e65e0332c81e8
Adam Chainz <me@adamj.eu>
2017-04-14 10:32:09 +0100

    Remove unused 'setup.py publish'

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@
 # -*- encoding:utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import os
 import re
-import sys
 
 from setuptools import setup
 
@@ -19,17 +17,6 @@ def get_version(filename):
 
 version = get_version('flake8_comprehensions.py')
 
-
-if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist bdist_wheel")
-    os.system("twine upload dist/*")
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    sys.exit()
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
