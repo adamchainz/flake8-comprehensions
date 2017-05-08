a01484ae1d8562ef48c1627df706d4aecf57e1e0
Adam Johnson <me@adamj.eu>
2017-05-08 21:38:30 +0100

    setup.py - remove weird line continuation

--- setup.py
+++ setup.py
@@ -36,8 +36,7 @@ setup(
     url='https://github.com/adamchainz/flake8-comprehensions',
     entry_points={
         'flake8.extension': [
-            'C40 = '
-            'flake8_comprehensions:ComprehensionChecker',
+            'C40 = flake8_comprehensions:ComprehensionChecker',
         ],
     },
     py_modules=['flake8_comprehensions'],
