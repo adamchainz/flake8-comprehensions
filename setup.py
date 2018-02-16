8a6679eeb2f8700c2532684bb29a472667e43796
Adam Johnson <me@adamj.eu>
2018-02-16 10:36:04 -0500

    Support Python 3.4

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         'flake8!=3.2.0',
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license="ISCL",
     zip_safe=False,
     keywords='flake8, comprehensions, list comprehension, set comprehension, '
