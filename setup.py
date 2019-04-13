e4c85dde9ad53ca5c89242e24f151b7ddad3db5d
Adam Johnson <me@adamj.eu>
2019-04-13 14:24:50 +0200

    Alphabetically sort trove classifiers

--- setup.py
+++ setup.py
@@ -48,11 +48,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
     ],
 )
