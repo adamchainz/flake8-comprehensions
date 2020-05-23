85c07f3743cd5da4f5753e21c98cdfbbe6d9ddb8
Adam Johnson <me@adamj.eu>
2020-05-23 19:45:38 +0100

    black

--- tests/test_flake8_comprehensions.py
+++ tests/test_flake8_comprehensions.py
@@ -14,7 +14,7 @@ python_3_6_plus = pytest.mark.skipif(sys.version_info < (3, 6), reason="Python 3
 def test_version(flake8dir):
     result = flake8dir.run_flake8(["--version"])
     version_string = "flake8-comprehensions: " + version("flake8-comprehensions")
-    unwrapped = ''.join(result.out_lines)
+    unwrapped = "".join(result.out_lines)
     assert version_string in unwrapped
 
 
